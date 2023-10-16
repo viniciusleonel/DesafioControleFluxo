dea68f0c0b05c82267419325f10211d64f392bb3
Vinicius Leonel <viniciuslps.cms@gmail.com>
2023-10-16 20:35:30 -0300

    melhorando impressão

--- src/Contador.java
+++ src/Contador.java
@@ -26,7 +26,7 @@ public class Contador {
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
         for (int i = 0; i < contagem; i++) {
-            System.out.println("Imprimindo o nº " + i);
+            System.out.println("Imprimindo o nº " + (i+1));
         }
 	}
 }
